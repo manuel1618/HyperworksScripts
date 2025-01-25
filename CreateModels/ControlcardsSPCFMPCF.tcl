251390afb2ab37aa07f054b6e82047ec8594219f
Manuel <manuel.ramsaier89@gmail.com>
2025-01-25 22:08:03 +0100

    Create ControlcardsSPCFMPCF.tcl (transferred from mpc forces extractor)

---
+++ CreateModels/ControlcardsSPCFMPCF.tcl
@@ -0,0 +1,120 @@
+*clearmark results 1
+*cardcreate "GLOBAL_OUTPUT_REQUEST"
+*clearmark cards 1
+*setvalue cards id=1 STATUS=2 2427=1
+*setvalue cards id=1 STATUS=2 3321=0
+*setvalue cards id=1 STATUS=2 10184=0
+*setvalue cards id=1 STATUS=2 10189=0
+*setvalue cards id=1 STATUS=2 9792=0
+*setvalue cards id=1 STATUS=2 12155=0
+*setvalue cards id=1 STATUS=2 12158=0
+*setvalue cards id=1 STATUS=2 12160=0
+*setvalue cards id=1 STATUS=2 12162=0
+*setvalue cards id=1 STATUS=2 9630=0
+*setvalue cards id=1 STATUS=2 9307=0
+*setvalue cards id=1 STATUS=2 9317=0
+*setvalue cards id=1 STATUS=2 9327=0
+*setvalue cards id=1 STATUS=2 3880=0
+*setvalue cards id=1 STATUS=2 4119=0
+*setvalue cards id=1 STATUS=2 4114=0
+*setvalue cards id=1 STATUS=2 7121=0
+*setvalue cards id=1 STATUS=2 2938=0
+*setvalue cards id=1 STATUS=2 10688=0
+*setvalue cards id=1 STATUS=2 523=0
+*setvalue cards id=1 STATUS=2 2385=0
+*setvalue cards id=1 STATUS=2 4052=0
+*setvalue cards id=1 STATUS=2 3712=0
+*setvalue cards id=1 STATUS=2 3885=0
+*setvalue cards id=1 STATUS=2 274=0
+*setvalue cards id=1 STATUS=2 3057=0
+*setvalue cards id=1 STATUS=2 10833=0
+*setvalue cards id=1 STATUS=2 7113=0
+*setvalue cards id=1 STATUS=2 8500=0
+*setvalue cards id=1 STATUS=2 2419=0
+*setvalue cards id=1 STATUS=2 8493=0
+*setvalue cards id=1 STATUS=2 9709=0
+*setvalue cards id=1 STATUS=2 3809=0
+*setvalue cards id=1 STATUS=2 12400=0
+*setvalue cards id=1 STATUS=2 12404=0
+*setvalue cards id=1 STATUS=2 7125=0
+*setvalue cards id=1 STATUS=2 4877=0
+*setvalue cards id=1 STATUS=2 12775=0
+*setvalue cards id=1 STATUS=2 9337=0
+*setvalue cards id=1 STATUS=2 9347=0
+*setvalue cards id=1 STATUS=2 9357=0
+*setvalue cards id=1 STATUS=2 3325=0
+*setvalue cards id=1 STATUS=2 7093=0
+*setvalue cards id=1 STATUS=2 3333=0
+*setvalue cards id=1 STATUS=2 2423=0
+*setvalue cards id=1 STATUS=2 4047=0
+*setvalue cards id=1 STATUS=2 10701=0
+*setvalue cards id=1 STATUS=2 9275=0
+*setvalue cards id=1 STATUS=2 5463=0
+*setvalue cards id=1 STATUS=2 8949=0
+*setvalue cards id=1 STATUS=2 10504=0
+*setvalue cards id=1 STATUS=2 12336=0
+*setvalue cards id=1 STATUS=2 10440=0
+*setvalue cards id=1 STATUS=2 7329=0
+*setvalue cards id=1 STATUS=2 7333=0
+*setvalue cards id=1 STATUS=0 1921=1
+*setvalue cards id=1 STATUS=2 ROW=0 9609= { }
+*setvalue cards id=1 STATUS=2 ROW=0 4323= { }
+*setvalue cards id=1 STATUS=2 ROW=0 3342= { }
+*setvalue cards id=1 STATUS=2 ROW=0 12602= { }
+*setvalue cards id=1 STATUS=2 ROW=0 3343= { }
+*setvalue cards id=1 STATUS=2 ROW=0 2294= { }
+*setvalue cards id=1 STATUS=0 3437={analysisparameters 0}
+*setvalue cards id=1 STATUS=2 ROW=0 11389= { }
+*setvalue cards id=1 STATUS=2 ROW=0 2428= {ALL}
+*setvalue cards id=1 STATUS=2 8153=0
+*setvalue cards id=1 STATUS=2 8150=0
+*setvalue cards id=1 STATUS=2 8144=0
+*setvalue cards id=1 STATUS=2 3642=0
+*setvalue cards id=1 STATUS=2 2431=0
+*setvalue cards id=1 STATUS=2 7337=0
+*setvalue cards id=1 STATUS=2 7117=0
+*setvalue cards id=1 STATUS=2 4126=0
+*setvalue cards id=1 STATUS=2 3891=0
+*setvalue cards id=1 STATUS=2 12346=0
+*setvalue cards id=1 STATUS=2 3329=0
+*mergehistorystate "" ""
+*setvalue cards id=1 STATUS=2 1921=2
+*setvalue cards id=1 STATUS=2 ROW=0 9609= { }
+*setvalue cards id=1 STATUS=2 ROW=1 9609= { }
+*setvalue cards id=1 STATUS=2 ROW=0 4323= { }
+*setvalue cards id=1 STATUS=2 ROW=1 4323= { }
+*setvalue cards id=1 STATUS=2 ROW=0 3342= { }
+*setvalue cards id=1 STATUS=2 ROW=1 3342= { }
+*setvalue cards id=1 STATUS=2 ROW=0 12602= { }
+*setvalue cards id=1 STATUS=2 ROW=1 12602= { }
+*setvalue cards id=1 STATUS=2 ROW=0 3343= { }
+*setvalue cards id=1 STATUS=2 ROW=1 3343= { }
+*setvalue cards id=1 STATUS=2 ROW=0 2294= { }
+*setvalue cards id=1 STATUS=2 ROW=1 2294= { }
+*setvalue cards id=1 STATUS=0 3437={analysisparameters 0 0}
+*setvalue cards id=1 STATUS=2 ROW=0 11389= { }
+*setvalue cards id=1 STATUS=2 ROW=1 11389= { }
+*setvalue cards id=1 STATUS=2 ROW=0 2428= {ALL}
+*setvalue cards id=1 STATUS=2 ROW=1 2428= {ALL}
+*mergehistorystate "" ""
+*setvalue cards id=1 STATUS=2 ROW=0 4323= { }
+*setvalue cards id=1 STATUS=2 ROW=1 4323= {OPTI}
+*setvalue cards id=1 STATUS=2 3325=1
+*setvalue cards id=1 STATUS=0 1914=1
+*setvalue cards id=1 STATUS=2 ROW=0 9618= { }
+*setvalue cards id=1 STATUS=2 ROW=0 4320= { }
+*setvalue cards id=1 STATUS=2 ROW=0 2293= { }
+*setvalue cards id=1 STATUS=2 ROW=0 3327= {ALL}
+*mergehistorystate "" ""
+*setvalue cards id=1 STATUS=2 1914=2
+*setvalue cards id=1 STATUS=2 ROW=0 9618= { }
+*setvalue cards id=1 STATUS=2 ROW=1 9618= { }
+*setvalue cards id=1 STATUS=2 ROW=0 4320= { }
+*setvalue cards id=1 STATUS=2 ROW=1 4320= { }
+*setvalue cards id=1 STATUS=2 ROW=0 2293= { }
+*setvalue cards id=1 STATUS=2 ROW=1 2293= { }
+*setvalue cards id=1 STATUS=2 ROW=0 3327= {ALL}
+*setvalue cards id=1 STATUS=2 ROW=1 3327= {ALL}
+*mergehistorystate "" ""
+*setvalue cards id=1 STATUS=2 ROW=0 4320= { }
+*setvalue cards id=1 STATUS=2 ROW=1 4320= {OPTI}
